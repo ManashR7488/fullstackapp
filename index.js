23b0d16463cc517bb6cfe3cf6bf3dc276533350c
Manash Ranjan <mahanandmanash91@gmail.com>
2024-06-16 20:10:17 +0530

    re configured

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-require('dotenv').config();
-const port = 3000 || process.env.PORT
+const path = require("path");
+require("dotenv").config();
+const port = 3000 || process.env.PORT;
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res){
-res.render('index');
-})
-
-app.get('/admin', function(req, res){
-    res.send("admin")
+app.get("/", function (req, res) {
+  res.render("index");
+  console.log("initialized");
 });
 
-app.listen(port, function(){
- console.log(`Example app listening on port ${port}!`);
-})
+app.get("/admin", function (req, res) {
+  res.send("admin");
+});
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
