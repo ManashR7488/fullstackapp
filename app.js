ab2a6ecaece551d76796972bccf0285333c8b115
Manash Ranjan <mahanandmanash91@gmail.com>
2024-06-16 18:14:35 +0530

    template express created

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+require('dotenv').config();
+const port = 3000 || process.env.PORT
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+// res.send('Hello World');
+res.render('index')
+})
+
+
+app.listen(port, function(){
+ console.log(`Example app listening on port ${port}!`);
+})
